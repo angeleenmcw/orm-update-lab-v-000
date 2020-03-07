55674774c144a55e3be20d303b220a0941ef8004
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-03-07 05:58:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,7 +31,7 @@ class Student
   def save
     sql = <<-SQL 
     INSERT INTO students(name, grade)
-    VALUES (? , ?)
+    VALUES ("Sarah" , 9)
     SQL
     
     DB[:conn].execute(sql, self.name, self.grade)
