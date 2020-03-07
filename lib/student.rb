248680377804b11c8cff9b7270080d7fdb7075e0
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-03-07 05:53:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ class Student
     SQL
     
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() students")
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")
   end
   
   def self.create(name:, grade:)
